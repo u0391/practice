4c6afa4828d7fe37a010eb78725296e9ce83a9fb
Murakami Nariaki <n-murakami@acesystems.co.jp>
2021-09-01 14:32:46 +0900

    9/1学習内容

--- Rensyuu_5_1_1.java
+++ Rensyuu_5_1_1.java
@@ -12,26 +12,53 @@ public class Rensyuu_5_1_1 {
 		
 		String xx= b.readLine();
 		
-//		String yy= b.readLine();
+		String yy= b.readLine();
+		
+		String zz= b.readLine();
+		
 		
 		int x=Integer.parseInt(xx);
 		
-//		int y=Integer.parseInt(yy);
+		int y=Integer.parseInt(yy);
 		
+		int z=Integer.parseInt(zz);
 		
-		if(x>=0) {
-			if(x%2==0) {
-				System.out.println("正の偶数");
-			}else {
-				System.out.println("正の奇数");
-			}
+		System.out.println("ケース１");
+
+		if(x>=60) {
+			System.out.println("合格");
 		}else {
-			if(x%2==0) {
-				System.out.println("負の偶数");
-			}else {
-				System.out.println("負の奇数");
-			}
+			System.out.println("不合格");
 		}
+		
+		System.out.println("ケース２");
+		
+		if(y>=80) {
+			System.out.println("たいへんよくできました");
+		
+		}else if(y>=60 && y<80) {
+			System.out.println("よくできました");
+		
+		}else if(y<60) {
+			System.out.println("残念でした");
+		}
+		
+		System.out.println("ケース３");	
+			
+		if(z>=80) {
+			System.out.println("優");
+			
+		}else if(z>=70 && z<80) {
+			System.out.println("良");
+			
+		}else if(z>=60 && z<70) {
+			System.out.println("可");
+			
+		}else if(z<60) {
+			System.out.println("不可");
+		}
+		
+		
 
 	}
 
