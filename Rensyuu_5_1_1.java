81e694673384370b0a49d50896910d3ccf9c6978
Murakami Nariaki <n-murakami@acesystems.co.jp>
2021-09-02 11:04:52 +0900

    9/2学習内容

--- Rensyuu_5_1_1.java
+++ Rensyuu_5_1_1.java
@@ -22,6 +22,22 @@ public class Rensyuu_5_1_1 {
 		if (x<y && x%2==0 && y%2==0) {
 			
 			System.out.println("xはyより小さく、かつ、xとyは共に偶数である");
+			
+		}else if(x==y && x<0 && y<0) {
+			
+			System.out.println("xとyは等しく、かつ、負の数である");
+			
+		}else if(x<y || x%2==0) {
+			
+			System.out.println("xはyより小さい、または、xは偶数である");
+			
+		}else if(x<=10 || x>=100 && y>=10 && y<=100) {
+			
+			System.out.println("xは10以下または100以上で、かつ、ｙは１０以上かつ１００以下である。");
+		
+		}else if(!(x<0) && !(y<0)) {
+			
+			System.out.println("ｘもｙも負の数である、ではない");
 		}
 		
 
